45e1f0b2347815ec4ff131657eb7c57e1ac5934f
Christopher Kampe <Kampe.christopher@gmail.com>
2017-05-20 11:39:13 -0400

    compiles; working autotile *bottom row issue

---
+++ Terrain_Translator/src/Test.java
@@ -0,0 +1,11 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+			RPG_Maker_Map test = new RPG_Maker_Map();
+			test.initialize();
+
+			}// TODO Auto-generated method stub
+	}
+
